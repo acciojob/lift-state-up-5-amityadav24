78e7dbc89a01830fe3abd027bb43fa677de012fc
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-06-29 14:02:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-// Get the root DOM node
-const container = document.getElementById("root");
-
-// Create a root and render App
-const root = createRoot(container);
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
