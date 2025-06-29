a290edff7bdaf8afc547f88aa4ada048e0285793
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-06-29 14:03:44 +0530

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,37 @@
 import React, { useState } from "react";
 
-const LoginForm = ({ onLogin }) => {
+const LoginForm = ({ setIsLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onLogin();
+    // Basic logic to trigger login
+    if (username && password) {
+      setIsLoggedIn(true);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      /><br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      /><br />
+      <label>
+        Username:{" "}
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:{" "}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
       <button type="submit">Login</button>
     </form>
   );
